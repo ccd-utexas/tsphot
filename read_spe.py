7c242b9d8d761530f055aa0740f9db7909bd28cd
ccd-admin <ccd.utexas@gmail.com>
2014-05-23 00:57:00 +0000

    datetime can't handle huge microseconds

--- read_spe.py
+++ read_spe.py
@@ -334,10 +334,12 @@ class File(object):
         mtsexpend_offset = mtsexpstart_offset + bytes_per_metadata_elt
         mftracknum_offset = mtsexpend_offset + bytes_per_metadata_elt
         metadata = {}
-        mtsexpstart = dt.timedelta(
-            microseconds=(self._read_at(mtsexpstart_offset, 1, File._metadata_ntype)[0]).astype(long))
-        mtsexpend   = dt.timedelta(
-            microseconds=(self._read_at(mtsexpend_offset, 1, File._metadata_ntype)[0]).astype(long))
+#        mtsexpstart = dt.timedelta(
+#            microseconds=(self._read_at(mtsexpstart_offset, 1, File._metadata_ntype)[0]).astype(int))
+#        mtsexpend   = dt.timedelta(
+#            microseconds=(self._read_at(mtsexpend_offset, 1, File._metadata_ntype)[0]).astype(int))
+        mtsexpstart = self._read_at(mtsexpstart_offset, 1, File._metadata_ntype)[0]
+        mtsexpend   = self._read_at(mtsexpend_offset, 1, File._metadata_ntype)[0]
         mftracknum  = self._read_at(mftracknum_offset, 1, File._metadata_ntype)[0]
 #        metadata["time_stamp_exposure_started"] = fctime + mtsexpstart
 #        metadata["time_stamp_exposure_ended"] = fctime + mtsexpend
