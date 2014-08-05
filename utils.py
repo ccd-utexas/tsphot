54f760e2c97d6a0b15f9916cabf5abd51532897d
stharrold <samuel.harrold@gmail.com>
2014-08-05 18:27:09 -0500

    utils works

--- utils.py
+++ utils.py
@@ -8,6 +8,7 @@ import os
 import sys
 import csv
 import pickle
+import inspect
 import astropy
 import ccdproc
 import read_spe
@@ -65,3 +66,60 @@ def get_exptime_prog(spe_footer_xml):
     exptime_prog = int(footer_xml.find(name='ExposureTime').contents[0])
     exptime_prog_res = int(footer_xml.find(name='DelayResolution').contents[0])
     return (exptime_prog / exptime_prog_res)
+
+def reduce_ccddata_dict(dobj, bias=None, dark=None, flat=None,
+                        dobj_exptime=None, dark_exptime=None, flat_exptime=None):
+    """
+    Reduce a dict of object data frames using the master calibration frames
+    for bias, dark, and flats. All frames must be type ccdproc.CCDData.
+    Requires exposure times (seconds) for object data frames, master dark, and master flat.
+    Operations (from sec 4.5, Basic CCD Reduction, of Howell, 2006, Handbook of CCD Astronomy):
+    - subtract master bias from master dark
+    - subtract master bias from master flat
+    - scale and subract master dark from master flat
+    - subtract master bias from object
+    - scale and subtract master dark from object
+    - divide object by normalized master flat
+    """
+    # TODO:
+    # - parallelize
+    # - Compute and correct ccdgain
+    # - Remove cosmic rays.
+    #   STH, 20140805
+    # Check input.
+    iframe = inspect.currentframe()
+    (args, varargs, keywords, ilocals) = inspect.getargvalues(iframe)
+    for arg in args:
+        if ilocals[arg] == None:
+            print(("INFO: {arg} is None.").format(arg=arg))
+    # Operations:
+    # - subtract master bias from master dark
+    # - subtract master bias from master flat
+    if bias != None:
+        if dark != None:
+            dark = ccdproc.subtract_bias(dark, bias)
+        if flat != None:
+            flat = ccdproc.subtract_bias(flat, bias)
+    # Operations:
+    # - scale and subract master dark from master flat
+    if ((dark != None) and
+        (flat != None)):
+        flat = ccdproc.subtract_dark(flat, dark,
+                                     dark_exposure=dark_exptime,
+                                     data_exposure=flat_exptime,
+                                     scale=True)
+    # Operations:
+    # - subtract master bias from object
+    # - scale and subtract master dark from object
+    # - divide object by normalized master flat
+    for fidx in dobj:
+        if isinstance(dobj[fidx], ccdproc.CCDData):
+            if bias != None:
+                dobj[fidx] = ccdproc.subtract_bias(dobj[fidx], bias)
+            if dark != None:
+                dobj[fidx] = ccdproc.subtract_dark(dobj[fidx], dark,
+                                                   dark_exposure=dark_exptime,
+                                                   data_exposure=dobj_exptime)
+            if flat != None:
+                dobj[fidx] = ccdproc.flat_correct(dobj[fidx], flat)
+    return dobj
