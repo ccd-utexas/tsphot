4acc68a7e16cbb503a8663b0b56e43cfa74b8937
stharrold <samuel.harrold@gmail.com>
2014-08-09 13:10:49 -0500

    made dataframes

--- utils.py
+++ utils.py
@@ -17,7 +17,8 @@ import ccdproc
 import imageutils
 import photutils
 from astroML import stats
-from skimage import feature, measure
+import scipy
+from skimage import feature
 import matplotlib.pyplot as plt
 
 def create_config(fjson='config.json'):
@@ -171,12 +172,30 @@ def normalize(array):
     median = np.median(array)
     return (array - median) / sigmaG
 
-def detect_blobs(image,
-                 blobargs=dict(min_sigma=1, max_sigma=1, num_sigma=1, threshold=2)):
-    """Detect blobs in an image.
+def sigma_to_fwhm(sigma):
+    """Convert the standard deviation sigma of a Gaussian into
+    the full-width-at-half-maximum.
+
+    Parameters
+    ----------
+    sigma : number_like
+        ``number_like``, e.g. ``float`` or ``int``
+
+    References
+    ----------
+    .. [1] http://en.wikipedia.org/wiki/Full_width_at_half_maximum
+    
+    """
+    fwhm = 2.0*math.sqrt(2.0*np.log(2.0))*sigma
+    return fwhm
+
+def find_stars(image,
+               blobargs=dict(min_sigma=1, max_sigma=1, num_sigma=1, threshold=2)):
+    """Find stars in an image and return as a dataframe.
     
     Function normalizes the image [1]_ then uses Laplacian of Gaussian method [2]_ [3]_
-    to find source-like blobs. Method finds stars and galaxies.
+    to find star-like blobs. Method can also find galaxies by modifying `blobargs`,
+    however this pipeline is taylored for stars.
     
     Parameters
     ----------
@@ -184,58 +203,51 @@ def detect_blobs(image,
         2D array of image.
     blobargs : {dict(min_sigma=1, max_sigma=1, num_sigma=1, threshold=2)}, optional
         Dict of keyword arguments for `skimage.feature.blob_log` [3]_.
-        Can generalize to extended sources but for reduced performance
+        Can generalize to extended sources but for increased execution time.
         On 256x256 image: for example below, 0.33 sec/frame; for default above, 0.02 sec/frame.
         Because image is normalized, `threshold` is the number of stdandard deviations
         above background for counts per pixel.
-        Example: `blobargs=dict(min_sigma=1, max_sigma=30, num_sigma=10, threshold=2)`
+        Example for galaxies: `blobargs=dict(min_sigma=1, max_sigma=30, num_sigma=10, threshold=2)`
     
     Returns
     -------
-    blobs : pandas.DataFrame
-        ``pandas.DataFrame`` with 1 row for each blob and columns `x_blob`', `y_blob`, `sigma_blob`.
-        `sigma_blob` is the standard deviation of the Gaussian kernel that detected the blob (usually 1 pixel).
-    
-    Notes
-    -----
-    Blob radius is ~`sqrt(2)*sigma_blob` [3]_.
-    Blob radius and `sigma_blob` are not robust estimators of seeing FWHM.
+    stars : pandas.DataFrame
+        ``pandas.DataFrame`` has 1 row for each star.
+        Columns:
+            `x_pix` : x-coordinate of found star in image pixels.
+            `y_pix` : y-coordinate of found star in image pixels.
+            `sigma_pix` : Standard deviation of the Gaussian kernel that detected the blob (usually 1 pixel).
     
     References
     ----------
     .. [1] Ivezic et al, 2014, "Statistics, Data Mining, and Machine Learning in Astronomy",
-        sec 3.2, "Descriptive Statistics"
+           sec 3.2, "Descriptive Statistics"
     .. [2] http://scikit-image.org/docs/dev/auto_examples/plot_blob.html
     .. [3] http://scikit-image.org/docs/dev/api/skimage.feature.html#skimage.feature.blob_log
     
     """
+    # Normalize image then find stars. Order by x,y,sigma.
     image_normd = normalize(image)
-    blobs = pd.DataFrame(feature.blob_log(image_normd, **blobargs), columns=['y_blob', 'x_blob', 'sigma_blob'])
-    return blobs[['x_blob', 'y_blob', 'sigma_blob']]
+    stars = pd.DataFrame(feature.blob_log(image_normd, **blobargs),
+                         columns=['y_pix', 'x_pix', 'sigma_pix'])
+    return stars[['x_pix', 'y_pix', 'sigma_pix']]
 
-def plot_detected_blobs(image, blobs):
-    """Plot detected blobs overlayed on image.
+def plot_stars(image, stars):
+    """Plot detected stars overlayed on image.
 
-    Overlay image markers for blobs. Convert blob sigma to radius.
-    From skimage gallery [1]_.
+    Overlay image markers for stars. Convert star sigma to radius.
     
     Parameters
     ----------
     image : array_like
         2D array of image.
-    blobs : pandas.DataFrame
-        ``pandas.DataFrame`` with 1 row for each blob and includes columns `x_blob`, `y_blob`, `sigma_blob`.
-        `sigma_blob` is the standard deviation of the Gaussian kernel that detected the blob (usually 1 pixel).
+    stars : pandas.DataFrame
+        ``pandas.DataFrame`` with 1 row for each star and with columns `x_pix`, `y_pix`, `sigma_pix`.
 
     Returns
     -------
     None
-    
-    Notes
-    -----
-    Blob radius is ~`sqrt(2)*sigma_blob` [2]_.
-    Blob radius and `sigma_blob` are not robust estimators of seeing FWHM.
-    
+        
     References
     ----------
     .. [1] http://scikit-image.org/docs/dev/auto_examples/plot_blob.html
@@ -243,88 +255,98 @@ def plot_detected_blobs(image, blobs):
     
     """
     (fig, ax) = plt.subplots(1, 1)
-    ax.imshow(image, interpolation='nearest')
-    for (idx, x_blob, y_blob, sigma_blob) in blobs[['x_blob', 'y_blob', 'sigma_blob']].itertuples():
-        radius_blob = sigma_blob*math.sqrt(2)
-        c = plt.Circle((x_blob, y_blob), radius_blob, color='yellow', linewidth=2, fill=False)
-        ax.add_patch(c)
+    ax.imshow(image, interpolation='none')
+    for (idx, x_pix, y_pix, sigma_pix) in stars[['x_pix', 'y_pix', 'sigma_pix']].itertuples():
+        fwhm_pix = sigma_to_fwhm(sigma_pix)
+        radius_pix = fwhm_pix / 2.0
+        circle = plt.Circle((x_pix, y_pix), radius=radius_pix,
+                            color='yellow', linewidth=2, fill=False)
+        ax.add_patch(circle)
     plt.show()
 
-def center_stars(image, blobs, box_sigma=5, method='fit_bivariate_normal'):
-    """Find star centroids in an image with identified stars.
+def center_stars(image, stars, box_sigma=5, method='fit_bivariate_normal'):
+    """Compute star centroids in an image with identified stars.
 
-    Extract a subframe around each star. Length of the subframe is a factor of star sigma [2]_.
-    Use a method to find the centroid. Return the dataframe with new columns for sub-pixel
+    Extract a subframe around each star. Length of the subframe box is a factor of star sigma in pixels.
+    From the given method, return the dataframe with refined coordinates and sigma in new columns.
+    new category `center_stars `x_pix`, `y_pix`, `sigma_pix`for sub-pixel
     x-, y-coordinates of centroid and standard deviation sigma of intensity distribution.
 
     Parameters
     ----------
     image : array_like
         2D array of image.
-    blobs : pandas.DataFrame
-        ``pandas.DataFrame`` with 1 row for each blob and includes columns `x_blob`, `y_blob`, `sigma_blob`.
-        `sigma_blob` is the standard deviation of the Gaussian kernel that detected the blob (usually 1 pixel).
-    box_sigma : {5}, optional
+    stars : pandas.DataFrame
+        ``pandas.DataFrame`` with 1 row for each star and includes columns `x`, `y`, `sigma`.
+        `sigma` is the standard deviation of the Gaussian kernel that detected the star (usually 1 pixel).
+    box_sigma : {5}, int, optional
         The dimensions for a subframe around the source for centering are
-        `box_sigma*sigma_blob` x `box_sigma*sigma_blob`. `sigma_blob` is usually 1 pixel.
-        Number should be odd so that center pixel of subframe is initial `x_blob`, `y_blob`.
-    method : {max_flux, daofind, irafstarfind, moments}, optional
+        `box_sigma*sigma` x `box_sigma*sigma`. `sigma` is from `stars` (usually 1 pixel).
+        Number should be odd so that center pixel of subframe is initial `x`, `y`.
+    method : {fit_bivariate_normal}, optional
         The method by which to compute the centroids.
-        `max_flux` : Return the centroid that yields the highest flux from photoemtry (from Mike Montgomery, UT Austin, 2014).
-        `moments` : Return the centroid from the image moments [3]_.
-
+        `fit_bivariate_normal` : Return the centroid and sigma from robustly fitting a bivariate normal distribution [1]_, [2]_.
     Returns
     -------
-    blobs : pandas.DataFrame
-        ``pandas.DataFrame`` with 1 row for each blob and added columns `x_ctrd`, `y_ctrd`.
-    
-    Notes
-    -----
-    Blob radius is ~`sqrt(2)*sigma_blob` [1]_.
-    Blob radius and `sigma_blob` are not robust estimators of seeing FWHM.
+    stars : pandas.DataFrame
+        ``pandas.DataFrame`` with 1 row for each star and added columns `x_ctrd`, `y_ctrd`, `sigma_ctrd`.
 
     TODO
     ----
-    Include Mike's max_phot_flux method, irafstarfind image moments method
-    daophot 2D Gaussian method.
+    Include other methods. https://github.com/ccd-utexas/tsphot/issues/69
+    Restructure columns to be nested: http://pandas.pydata.org/pandas-docs/stable/indexing.html#hierarchical-indexing-multiindex
     
     References
     ----------
-    .. [1] http://scikit-image.org/docs/dev/api/skimage.feature.html#skimage.feature.blob_log
-    .. [2] https://imageutils.readthedocs.org/en/latest/api/imageutils.extract_array_2d.html#imageutils.extract_array_2d
-    .. [3] http://scikit-image.org/docs/dev/api/skimage.measure.html#moments
+    .. [1] http://www.astroml.org/book_figures/chapter3/fig_robust_pca.html
+    .. [2] Ivezic et al, 2014, "Statistics, Data Mining, and Machine Learning in Astronomy",
+           sec 3.3.1., "The Uniform Distribution"
     
     """
     # Check input
     valid_methods = ['fit_bivariate_normal']
-    if method not in valid_methods:
-        raise IOError(("Invalid method: {meth}\n"+
-                       "Valid methods: {vmeth}").format(meth=method, vmeth=valid_methods))
-    # Make box subframes and compute centroids per chosed method. Each blob may have a different size.
-    # Subframe can be shortened due to proximity to frame edge.
-    (blobs['x_ctrd'], blobs['y_ctrd']) = (np.NaN, np.NaN)
-    for (idx, x_blob, y_blob, sigma_blob) in blobs[['x_blob', 'y_blob', 'sigma_blob']].itertuples():
-        width = np.round(box_sigma*sigma_blob)
+    assert method in valid_methods
+    # if method not in valid_methods:
+    #     raise IOError(("Invalid method: {meth}\n"+
+    #                    "Valid methods: {vmeth}").format(meth=method, vmeth=valid_methods))
+    # Make box subframes and compute centroids per chosed method. Each star may have a different sigma.
+    # Subframe may be shortened due to proximity to frame edge.
+    (stars['x_ctrd'], stars['y_ctrd'], stars['sigma_ctrd']) = (np.NaN, np.NaN, np.NaN)
+    for (idx, x_star, y_star, sigma_star) in stars[['x_star', 'y_star', 'sigma_star']].itertuples():
+        width = np.round(box_sigma*sigma_star)
         height = width
         # width, height order is reverse of position x, y order
         subframe = imageutils.extract_array_2d(array_large=image,
                                                shape=(height, width),
-                                               position=(x_blob, y_blob))
-        elif method == 'fit_bivariate_normal':
+                                               position=(x_star, y_star))
+        if method == 'fit_bivariate_normal':
             # Model the photons hitting the pixels of the subframe and
             # robustly fit a bivariate normal distribution.
-            # Photons hit each pixel with a uniform distribution (sec 3.3).
-            # http://www.astroml.org/book_figures/chapter3/fig_robust_pca.html
-
-            
-            pass
+            # Photons hit each pixel with a uniform distribution. See [1]_, [2]_.
+            # Process takes ~90 ms per subframe.
+            x_dist = []
+            y_dist = []
+            for x_idx in xrange(len(subframe)):
+                for y_idx in xrange(len(subframe[x_idx])):
+                    pixel_counts = np.round(subframe[x_idx][y_idx])
+                    np.random.seed(0)
+                    x_pix_dist = scipy.stats.uniform(x_idx - 0.5, 1)
+                    x_dist.extend(x_pix_dist.rvs(pixel_counts))
+                    y_pix_dist = scipy.stats.uniform(y_idx - 0.5, 1)
+                    y_dist.extend(y_pix_dist.rvs(pixel_counts))
+            (mu, sigma1, sigma2, alpha) = stats.fit_bivariate_normal(x_dist, y_dist, robust=True)
+            (x_sub_ctrd, y_sub_ctrd) = mu
+            # Modeling coordinate (x,y) as sum of uncorrelated vectors x, y, so
+            # adding variances per sec 3.5.1 of Ivezic 2014 [2]_.
+            sigma_ctrd = math.sqrt(sigma1**2 + sigma2**2)
         else:
             raise AssertionError(("Program error. Input method not accounted for: {meth}").format(meth=method))
-        # Compute the centroid coordinates relative to the blob centers.
-        (x_sub_blob, y_sub_blob) = map(lambda dim: (np.round(dim) - 1)/2, (width, height))
-        (x_offset, y_offset) = (x_sub_ctrd - x_sub_blob,
-                                y_sub_ctrd - y_sub_blob)
-        blobs.loc[idx, ['x_ctrd', 'y_ctrd']] = (x_blob + x_offset,
-                                                y_blob + y_offset)
-    return blobs
+        # Compute the centroid coordinates relative to the star centers.
+        (x_sub_star, y_sub_star) = map(lambda dim: (np.round(dim) - 1)/2, (width, height))
+        (x_offset, y_offset) = (x_sub_ctrd - x_sub_star,
+                                y_sub_ctrd - y_sub_star)
+        stars.loc[idx, ['x_ctrd', 'y_ctrd', 'sigma_ctrd']] = (x_star + x_offset,
+                                                              y_star + y_offset,
+                                                              sigma_ctrd)
+    return stars
 
