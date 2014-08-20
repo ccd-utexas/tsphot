c6f00139c7f755ccc645816ed425c95c7bab8d70
stharrold <samuel.harrold@gmail.com>
2014-08-20 10:38:04 -0500

    adding logger
    reference #56

--- main_utils.py
+++ main_utils.py
@@ -30,6 +30,7 @@ from __future__ import division, absolute_import, print_function
 import os
 import json
 import pickle
+import logging
 import argparse
 import collections
 
@@ -64,28 +65,24 @@ def main(fconfig, rereduce=False, verbose=0):
     """
     # Read configuration file.
     # Use binary read-write for cross-platform compatibility. Use Python-style indents in the JSON file.
-    if verbose >= 1:
-        print("INFO: Reading configuration file {fconfig}".format(fconfig=fconfig))
+    # TODO: remove verbose variable
+    logging.info("Reading configuration file {fconfig}".format(fconfig=fconfig))
     with open(fconfig, 'rb') as fp:
         config_settings = json.load(fp, object_pairs_hook=collections.OrderedDict)
-    if verbose >= 2:
-        print("DEBUG: Configuration file settings: {settings}".format(settings=config_settings))
+    logging.debug("Configuration file settings: {settings}".format(settings=config_settings))
     # Check configuration file.
     utils.check_config(dobj=config_settings)
     # Create master calibration (calib.)frames.
     # Use binary read-write for cross-platform compatibility. Use Python-style indents in the JSON file.
     # TODO: parallelize
-    # TODO: logging
-    if verbose >= 1:
-        print("INFO: Creating master calibration files.")
+    logging.info("Creating master calibration files.")
     master_ccddata = {}
     master_fpath = config_settings['master']
     # If master calib. frame files already exists, load them. Otherwise initialize master calib. frame as None.
     for imtype in master_fpath:
         mfpath = master_fpath[imtype]
         if (mfpath is not None) and os.path.isfile(mfpath):
-            if verbose >= 2:
-                print("DEBUG: Loading master calibration frame from: {fpath}".format(fpath=mfpath))
+            logging.debug("Loading master calibration frame from: {fpath}".format(fpath=mfpath))
             with open(mfpath, 'rb') as fp:
                 master_ccddata[imtype] = pickle.load(fp)
         else:
@@ -97,13 +94,11 @@ def main(fconfig, rereduce=False, verbose=0):
         cfpath = calib_fpath[imtype]
         mfpath = master_fpath[imtype]
         if master_ccddata[imtype] is None:
-            if verbose >= 2:
-                print("DEBUG: Creating master calibration frame from: {fpath}".format(fpath=cfpath))
+            logging.debug("Creating master calibration frame from: {fpath}".format(fpath=cfpath))
             dobj = utils.spe_to_dict(fpath=cfpath)
             master_ccddata[imtype] = utils.create_master_calib(dobj=dobj)
             if mfpath is not None:
-                if verbose >= 2:
-                    print("DEBUG: Writing master calibration frame to: {fpath}".format(fpath=mfpath))
+                logging.debug("Writing master calibration frame to: {fpath}".format(fpath=mfpath))
                 with open(mfpath, 'wb') as fp:
                     pickle.dump(master_ccddata[imtype], fp)
     # If re-reduce flag given, recreate all master calib. frames.
@@ -114,13 +109,11 @@ def main(fconfig, rereduce=False, verbose=0):
             mfpath = master_fpath[imtype]
             if (cfpath is None) or (not os.path.isfile(cfpath)):
                 raise IOError("Calibration frame file does not exist: {fpath}".format(fpath=cfpath))
-            if verbose >= 2:
-                print("DEBUG: Creating master calibration frame from: {fpath}".format(fpath=cfpath))
+            logging.debug("Creating master calibration frame from: {fpath}".format(fpath=cfpath))
             dobj = utils.spe_to_dict(fpath=cfpath)
             master_ccddata[imtype] = utils.create_master_calib(dobj=dobj)
             if mfpath is not None:
-                if verbose >= 2:
-                    print("DEBUG: Writing master calibration frame to: {fpath}".format(fpath=mfpath))
+                logging.debug("Writing master calibration frame to: {fpath}".format(fpath=mfpath))
                 with open(mfpath, 'wb') as fp:
                     pickle.dump(master_ccddata[imtype], fp)
     # TODO: resume here
@@ -128,29 +121,39 @@ def main(fconfig, rereduce=False, verbose=0):
 
 
 if __name__ == '__main__':
-    # TODO: use logging
-    defaults = {'fconfig': 'config.json'}
-    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
-                                     description="Read configuration file and reduce data.")
+    defaults = {'fconfig' : 'config.json',
+                'flog'    : 'tsphot.log',
+                'loglevel': 'INFO'}
+    parser = argparse.ArgumentParser(description="Read configuration file and reduce data.")
     parser.add_argument('--fconfig',
                         default=defaults['fconfig'],
-                        help=(("Input configuration file as .json.\n" +
+                        help=(("Input JSON configuration file.\n" +
                                "Default: {dflt}").format(dflt=defaults['fconfig'])))
+    parser.add_argument('--flog',
+                        default=defaults['flog'],
+                        help=(("Output event log file.\n" +
+                               "Default: {dflt}").format(dflt=defaults['flog'])))
+    parser.add_argument('--loglevel',
+                        default=defaults['loglevel'],
+                        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+                        help=(("Logging level. Determines verbosity threshold for event tracking.\n"+
+                               "Default: {dflt}").format(dflt=defaults['loglevel'])))
     parser.add_argument('--rereduce',
                         action='store_true',
                         help=("Re-reduce all files. Overwrite previously reduced files.\n" +
                               "If option omitted, use previously reduced files."))
     parser.add_argument('--verbose', '-v',
-                        action='count',
-                        help="Print 'INFO:' messages to stdout.")
+                        action='store_true',
+                        help="Print logging messages to stdout.")
     args = parser.parse_args()
+    logging.basicConfig(filename='tsphot.log', level=getattr(logging, args.loglevel.upper()))
+    logging.info("Arguments: {args}".format(args=args))
     if not os.path.isfile(args.fconfig):
         raise IOError("Configuration file does not exist: {fconfig}".format(fconfig=args.fconfig))
     (fbase, ext) = os.path.splitext(args.fconfig)
     if ext != '.json':
         raise IOError("Configuration file extension is not '.json': {fconfig}".format(fconfig=args.fconfig))
     if args.verbose:
-        print("INFO: Arguments:")
-        for arg in sorted(args.__dict__):
-            print('', arg, args.__dict__[arg])
-    main(**args.__dict__)
+        #TODO: set logging level
+        pass
+    main(fconfig=args.fconfig, rereduce=args.rereduce, verbose=args.verbose)
